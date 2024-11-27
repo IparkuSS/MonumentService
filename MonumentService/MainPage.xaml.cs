d88c77f6000404000faa14c642393457118c4e3f
iparkuSS <iparkuSS@mail.ru>
2024-11-27 17:15:49 +0300

    init app

---
+++ MonumentService/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MonumentService
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MonumentService/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MonumentService
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
